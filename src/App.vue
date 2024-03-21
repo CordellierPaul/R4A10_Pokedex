04223a5874613afd69fedca9c5144fbe49c40930
CordellierPaul <paul.cordellier@etu.univ-smb.fr>
2024-03-21 08:12:41 +0100

    Début

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import Pokemon from "./components/Pokemon.vue";
+
+const pokemons = ref(0)
+
+async function fetchPokemons() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+  pokemons.value = await response.json()
+}
+
+onMounted(fetchPokemons)
+</script>
+
+<template>
+  <div v-if="pokemons?.results">
+    <Pokemon v-for="pokemon of pokemons.results" :url="pokemon.url"></Pokemon>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const props = defineProps({
+    url: String
+})
+
+const pokemon = ref(0)
+
+async function fetchAPokemon() {
+  const response = await fetch(props.url)
+  pokemon.value = await response.json()
+}
+
+onMounted(fetchAPokemon)
+</script>
+
+<template>
+    <div v-if="pokemon">{{ pokemon.name }}</div>
+</template>
+
+<style scoped>
+
+</style>
